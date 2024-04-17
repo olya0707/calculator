aeacface0871dd11606fdca9fdebbb232c45a234
olya0707 <150253029+olya0707@users.noreply.github.com>
2024-04-17 22:24:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', e => {
+    switch (e.target.innerText) {
+      case 'AC':
+        display.value = '';
+        break;
+      case 'C':
+        display.value = display.value.slice(0, -1);
+        break;
+      case '=':
+        try {
+          display.value = eval(display.value);
+          if (isNaN(display.value) || !isFinite(display.value)) {
+            throw new Error('Invalid calculation');
+          }
+        } catch (e) {
+          display.value = 'Error!';
+        }
+        break;
+        case 'x²':
+          display.value = Math.pow(display.value, 2);
+          break;
+      case '√':
+        display.value = Math.sqrt(display.value, 2);
+        break;
+      case 'sin':
+        display.value = Math.sin(display.value);
+        break;
+      case 'cos':
+        display.value = Math.sqrt(display.value);
+        break;
+
+      case 'tan':
+        display.value = Math.sqrt(display.value);
+        break;
+      default:
+        if (display.value.length < 10) {
+          display.value += e.target.innerText;
+        }
+    }
+  });
+});
