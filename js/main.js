8f2ccc11ab6200af5b052b765bc3283021594a5b
olya0707 <150253029+olya0707@users.noreply.github.com>
2024-04-19 11:14:20 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,6 +37,9 @@ buttons.forEach(button => {
         display.value = Math.sqrt(display.value);
         break;
       default:
+        if (e.target.innerText === '.' && display.value.includes('.')) {
+                return;
+        }
         if (display.value.length < 10) {
           display.value += e.target.innerText;
         }
